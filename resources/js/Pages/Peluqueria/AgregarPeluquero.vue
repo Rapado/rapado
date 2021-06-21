3e6a38f6afc77da814da6508ff08fe38834771d6
Diego Gomez <juandie019@gmail.com>
2021-06-21 13:00:45 -0500

    se quito la transicion

--- resources/js/Pages/Peluqueria/AgregarPeluquero.vue
+++ resources/js/Pages/Peluqueria/AgregarPeluquero.vue
@@ -40,32 +40,28 @@
                             </form>
                         </div>
                         <div class="mt-6">
-                            <ul>
-                                <transition-group name="list" tag="p" >
-                                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-2 md:mx-0 justify-items-center">
-                                    <span class="list-item mt-4 w-full md:w-9/12" v-for="(peluquero, index) in peluquerosList" :key="index">
-                                        <peluquero-card :peluquero="peluquero" :cambiar-peluquero-estado="true" v-on:cambioEstado = "cambiarEstado">
-                                            <template v-slot:actions>
-                                                <div class="flex justify-center mt-1 md:mt-2">
-                                                <div id = "ver" v-if="!firstTime" class="ml-3" >
-                                                    <svg @click="verPeluquero(index)" class = "fill-current text-primary-dark hover:text-primary-darkest cursor-pointer" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>
+                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-2 md:mx-0 justify-items-center">
+                                <span class="mt-4 w-full md:w-9/12" v-for="(peluquero, index) in peluquerosList" :key="index">
+                                    <peluquero-card :peluquero="peluquero" :cambiar-peluquero-estado="true" v-on:cambioEstado = "cambiarEstado">
+                                        <template v-slot:actions>
+                                            <div class="flex justify-center mt-1 md:mt-2">
+                                            <div id = "ver" v-if="!firstTime" class="ml-3" >
+                                                <svg @click="verPeluquero(index)" class = "fill-current text-primary-dark hover:text-primary-darkest cursor-pointer" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>
+                                            </div>
+                                            <div id = "editar" class="ml-3">
+                                                <div class="cursor-pointer hover:text-secondary">
+                                                    <svg v-if="isEditting && index == edittingIndex"  @click="editarPeluqueroReset()" class = "fill-current text-secondary-light hover:text-secondary"  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
+                                                    <svg  v-else @click="editarPeluqueroSetUP(index)" class = "fill-current text-secondary-light hover:text-secondary" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
                                                 </div>
-                                                <div id = "editar" class="ml-3">
-                                                    <div class="cursor-pointer hover:text-secondary">
-                                                        <svg v-if="isEditting && index == edittingIndex"  @click="editarPeluqueroReset()" class = "fill-current text-secondary-light hover:text-secondary"  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
-                                                        <svg  v-else @click="editarPeluqueroSetUP(index)" class = "fill-current text-secondary-light hover:text-secondary" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
-                                                    </div>
-                                                </div>
-                                                <div id = "eliminar" class="ml-3" >
-                                                    <svg @click="eliminarPeluquero(index)" class = "fill-current text-error hover:text-accent-dark cursor-pointer" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
-                                                </div>
-                                                </div>
-                                            </template>
-                                        </peluquero-card>
-                                    </span>
-                                    </div>
-                                </transition-group>
-                            </ul>
+                                            </div>
+                                            <div id = "eliminar" class="ml-3" >
+                                                <svg @click="eliminarPeluquero(index)" class = "fill-current text-error hover:text-accent-dark cursor-pointer" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+                                            </div>
+                                            </div>
+                                        </template>
+                                    </peluquero-card>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -213,8 +209,6 @@
 </script>
 
 <style >
-
-
     .list-enter-active,
     .list-leave-active {
          transition: all 1s ease;
